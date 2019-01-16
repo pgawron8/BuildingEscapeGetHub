18b06480e8614d145df27d61db7610f1bddedfea
pgawron8 <peter.m.gawron@gmail.com>
2019-01-15 23:41:17 -0600

    Accessing info from game obj
    Making a component that sets the owning actor's rotation in yaw.

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -1,35 +1,35 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "PositionReport.h"
+#include "OpenDoor.h"
 #include "GameFramework/Actor.h"
-#include "Runtime/Core/Public/Math/Vector.h"
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UOpenDoor::UOpenDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
 	// ...
-	
 }
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	FString ObjName = GetOwner()->GetName();
-	FString ObjPos =  GetOwner()->GetActorTransform().GetLocation().ToString();//"X = somthing, Y = something, Z = Something";
+	AActor* Owner = GetOwner();
+
+	Owner->SetActorRotation(FRotator(0.0f, -50.0f, 0.0f));
+
 	// ...
-	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjName, *ObjPos);
+	
 }
 
 
 // Called every frame
-void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
